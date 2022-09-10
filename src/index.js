f3c184c7fc1bf32e26b1bec5a20d0e262c0fc9e7
flipott <ottphillipa@gmail.com>
2022-09-10 16:22:28 -0400

    Clean up code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import test from './ship';
+import Gameboard from './gameboard';
+import Player from './player';
+import Ship from './ship';
+import DisplayBoard from './displayBoard';
+
+const user = Player('Player', 'human');
+DisplayBoard(user.board.board, user.type);
+user.board.generateFleet();
+DisplayBoard(user.board.board, user.type);
