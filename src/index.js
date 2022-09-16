92f1cdb638591f67e9ffa32cffc29d2563aba998
flipott <ottphillipa@gmail.com>
2022-09-16 15:56:07 -0400

    Implement UI that works with basic game logic

--- src/index.js
+++ src/index.js
@@ -1,41 +1,89 @@
 import Ship from './ship.js';
 import Gameboard from './gameboard.js';
 import Player from './player.js';
-import DisplayBoard from './displayBoard.js';
+import Display from './displayBoard.js';
 
+const startButton = document.getElementById('game-start');
 let winner = null;
-console.log('HERE WE ARE');
-
-const cpu = Player('CPU', 'cpu');
+let cpu = Player('CPU', 'cpu');
 cpu.board.generateFleet();
-const player = Player('Player', 'human');
+let player = Player('Player', 'human');
 player.board.generateFleet();
 
-DisplayBoard(player.board.board, player.type);
-DisplayBoard(cpu.board.board, cpu.type);
+let cpuMoves = [];
+let playerMoves = [];
+
+Display.displayBoard(player.board.board, player.type);
+Display.displayBoard(cpu.board.board, cpu.type);
 
 let spaces = document.getElementsByClassName('space');
 
-function playerMove(x, y) {
-  console.log(`${x} ${y}`);
-  cpu.receiveAttack(x, y);
-  DisplayBoard(cpu.board.board, cpu.type);
+function init() {
+  winner = null;
+
+  cpu = Player('CPU', 'cpu');
+  cpu.board.generateFleet();
+  player = Player('Player', 'human');
+  player.board.generateFleet();
+
+  cpuMoves = [];
+  playerMoves = [];
+
+  Display.displayBoard(player.board.board, player.type);
+  Display.displayBoard(cpu.board.board, cpu.type);
+
   spaces = document.getElementsByClassName('space');
+}
+
+function domAttack() {
   for (let i = 0; i < 100; i += 1) {
     spaces[i].addEventListener('click', (e) => {
       playerMove(
-        parseInt(e.target.getAttribute('x'), 10),
-        parseInt(e.target.getAttribute('y'), 10)
+        parseInt(e.target.getAttribute('x')),
+        parseInt(e.target.getAttribute('y'))
       );
     });
   }
 }
 
-for (let i = 0; i < 100; i += 1) {
-  spaces[i].addEventListener('click', (e) => {
-    playerMove(
-      parseInt(e.target.getAttribute('x')),
-      parseInt(e.target.getAttribute('y'))
-    );
-  });
+function checkWinner(player, opponent) {
+  if (opponent.board.allSunk()) {
+    winner = player;
+    console.log('The winner is ' + player.name);
+    return true;
+  }
 }
+
+function opponentMove() {
+  const coords = cpu.sendRandomAttack();
+  cpuMoves.push(coords);
+  player.receiveAttack(coords[0], coords[1]);
+  Display.displayBoard(player.board.board, player.type);
+  checkWinner(cpu, player);
+}
+
+function playerMove(x, y) {
+  if (!winner) {
+    if (player.sendAttack(x, y)) {
+      const result = cpu.receiveAttack(x, y);
+      Display.displayResult(player.name, result);
+      Display.displayBoard(cpu.board.board, cpu.type);
+      if (!checkWinner(player, cpu)) {
+        spaces = document.getElementsByClassName('space');
+        setTimeout(function () {
+          opponentMove();
+        }, 250);
+
+        domAttack();
+      }
+    }
+  }
+}
+
+startButton.addEventListener('click', () => {
+  if (startButton.innerHTML === 'New game') {
+    init();
+  }
+  domAttack();
+  startButton.innerHTML = 'New game';
+});
